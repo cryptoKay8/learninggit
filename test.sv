10d0860aa0cde64211e868dd74064ae2318d303b
kevinRohan8 <krohan@andrew.cmu.edu>
2021-01-27 22:48:02 -0600

    merged from dev branch

--- test.sv
+++ test.sv
@@ -1,3 +1,6 @@
-module test(input a, output b);
-	assign b = a;
+//Converting this to a single bit dff
+module test_dff(input logic clk, input logic reset_n, input logic d, output logic q);
+	always_ff @(posedge clk, negedge reset_n) begin
+		q <= d;
+	end
 endmodule : test
